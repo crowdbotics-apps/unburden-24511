bd678d474bad99865bd9f56f8768e3449d756518
Crowdbotics <team@crowdbotics.com>
2021-02-14 02:13:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/unburden_24511/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unburden_24511;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "unburden_24511";
+    }
+}
---
+++ backend/unburden_24511/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for unburden_24511 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unburden_24511.settings')
+
+application = get_wsgi_application()
